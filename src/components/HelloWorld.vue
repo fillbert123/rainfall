9664fa8f934521e0a8be98108315cbb66003b4b5
Fillbert <fillbert.susanto@gdn-commerce.com>
2023-04-30 19:32:36 +0700

    add color variables

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -56,3 +56,18 @@ a {
   color: #42b983;
 }
 </style>
+
+<style lang="scss">
+:root {
+  --darker: rgba(49, 60, 68, 1)
+  --dark: rgba(72, 89, 100, 1)
+  --light: rgba(97, 118, 131, 1)
+  --lighter: rgba(127, 144, 153, 1)
+  --white: rgba(255, 255, 255, 1)
+  --darker_tr: rgba(24, 31, 35, 0.5)
+  --dark_tr: rgba(70, 88, 101, 0.5)
+  --light_tr: rgba(122, 147, 161, 0.5)
+  --lighter_tr: rgba(181, 198, 207, 0.5)
+  --lighter_tr2: rgba(181, 198, 207, 0.75)
+}
+</style>
