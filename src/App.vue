fa9e2e00a446c3c2b265c74ee573d348d692de3c
Fillbert <fillbert.susanto@gdn-commerce.com>
2023-04-30 20:06:59 +0700

    internationalization added

--- src/App.vue
+++ src/App.vue
@@ -24,15 +24,15 @@ export default {
 
 :root {
   --darker: rgba(49, 60, 68, 1)
-  --dark: rgba(72, 89, 100, 1)
-  --light: rgba(97, 118, 131, 1)
-  --lighter: rgba(127, 144, 153, 1)
-  --white: rgba(255, 255, 255, 1)
   --darker_tr: rgba(24, 31, 35, 0.5)
+  --dark: rgba(72, 89, 100, 1)
   --dark_tr: rgba(70, 88, 101, 0.5)
+  --light: rgba(97, 118, 131, 1)
   --light_tr: rgba(122, 147, 161, 0.5)
+  --lighter: rgba(127, 144, 153, 1)
   --lighter_tr: rgba(181, 198, 207, 0.5)
   --lighter_tr2: rgba(181, 198, 207, 0.75)
+  --white: rgba(255, 255, 255, 1)
 }
 
 #app {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueI18n from 'vue-i18n'
+import en from './i18n/en.json'
+import id from './i18n/id.json'
 
 Vue.config.productionTip = false
 
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: 'id',
+  messages: {
+    'en': en,
+    'id': id
+  }
+})
+
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
