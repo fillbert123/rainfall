d801b70dd6b049141a3c31f1deffcb0c34f3e9c2
Fillbert <fillbert.susanto@gdn-commerce.com>
2023-05-26 20:35:30 +0700

    DataSelector added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,12 +27,12 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-    <AreaModal class="modal"></AreaModal>
+    <DataSelector></DataSelector>
   </div>
 </template>
 
 <script>
-import AreaModal from '@/modals/AreaModal.vue'
+import DataSelector from './DataSelector.vue';
 
 export default {
   name: 'HelloWorld',
@@ -40,7 +40,7 @@ export default {
     msg: String
   },
   components: {
-    AreaModal
+    DataSelector
   }
 }
 </script>
@@ -60,8 +60,5 @@ li {
 }
 a {
   color: #42b983;
-}
-.modal {
-  zoom: 80%;
 }
 </style>
