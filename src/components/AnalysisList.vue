38d2b8847aec9106443ae584e08d73d65bad3116
Fillbert <fillbert.susanto@gdn-commerce.com>
2023-06-10 23:55:19 +0700

    AnalysisList added

---
+++ src/components/AnalysisList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="data">
+    <div class="data__list">
+      <img src="@/assets/icons/imputation.svg" alt="" class="data__list__icon">
+      <div class="data__list__text">
+        <div class="data__list__text__tag">Imputation</div>
+        <div class="data__list__text__label">Deterministic Regression Imputation</div>
+      </div>
+    </div>
+    <div class="data__list">
+      <img src="@/assets/icons/model.svg" alt="" class="data__list__icon">
+      <div class="data__list__text">
+        <div class="data__list__text__tag">Model</div>
+        <div class="data__list__text__label">Random Effect Model</div>
+        <div class="data__list__text__desc">Wallace-Hussain</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'analysisList'
+}
+</script>
+
+<style lang="scss" scoped>
+.data {
+  padding-inline-end: 12px;
+  padding-block-end: 12px;
+  width: fit-content;
+  border-radius: 24px;
+  background-color: var(--light_tr);
+  display: flex;
+  flex-direction: row;
+  &__list {
+    margin-inline-start: 12px;
+    margin-block-start: 12px;
+    padding: 12px;
+    color: var(--white);
+    width: 100px;
+    height: 100px;
+    background-color: var(--light_tr);
+    border-radius: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    &__text {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      &__tag {
+        font-family: SFRoundedR;
+        font-size: 12px;
+      }
+      &__label {
+        font-family: SFRoundedSB;
+        font-size: 16px;
+        text-align: left;
+      }
+      &__desc {
+        font-family: SFRoundedSB;
+        font-size: 12px;
+      }
+    }
+  }
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,14 +19,20 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <AnalysisList></AnalysisList>
   </div>
 </template>
 
 <script>
+import AnalysisList from './AnalysisList.vue';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    AnalysisList
   }
 }
 </script>
