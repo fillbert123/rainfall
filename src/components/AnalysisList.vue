32706d89e79fec225e1a6ebc13a6db539418a9af
Fillbert <fillbert.susanto@gdn-commerce.com>
2023-06-11 00:03:55 +0700

    AnalysisList added

--- src/components/AnalysisList.vue
+++ src/components/AnalysisList.vue
@@ -3,14 +3,22 @@
     <div class="data__list">
       <img src="@/assets/icons/imputation.svg" alt="" class="data__list__icon">
       <div class="data__list__text">
-        <div class="data__list__text__tag">Imputation</div>
+        <div class="data__list__text__tag">IMPUTATION</div>
         <div class="data__list__text__label">Deterministic Regression Imputation</div>
       </div>
     </div>
     <div class="data__list">
       <img src="@/assets/icons/model.svg" alt="" class="data__list__icon">
       <div class="data__list__text">
-        <div class="data__list__text__tag">Model</div>
+        <div class="data__list__text__tag">MODEL</div>
+        <div class="data__list__text__label">Random Effect Model</div>
+        <div class="data__list__text__desc">Swamy-Arora</div>
+      </div>
+    </div>
+    <div class="data__list">
+      <img src="@/assets/icons/model.svg" alt="" class="data__list__icon">
+      <div class="data__list__text">
+        <div class="data__list__text__tag">MODEL</div>
         <div class="data__list__text__label">Random Effect Model</div>
         <div class="data__list__text__desc">Wallace-Hussain</div>
       </div>
@@ -20,19 +28,24 @@
 
 <script>
 export default {
-  name: 'analysisList'
+  name: 'analysisList',
+  filters: {
+    uppercase (text) {
+      return text.toUpperCase()
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .data {
-  padding-inline-end: 12px;
   padding-block-end: 12px;
-  width: fit-content;
+  width: 284px;
   border-radius: 24px;
   background-color: var(--light_tr);
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   &__list {
     margin-inline-start: 12px;
     margin-block-start: 12px;
