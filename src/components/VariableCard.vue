04c9709b079bf4ae8819b31fa8205506ce418157
Fillbert <fillbert.susanto@gdn-commerce.com>
2023-06-12 11:43:43 +0700

    VariablePage added

--- src/components/VariableCard.vue
+++ src/components/VariableCard.vue
@@ -2,7 +2,7 @@
   <div class="data">
     <div class="data__upper">
       <div class="data__upper__icon">
-        <img class="data__upper__icon__image" src="@/assets/icons/humidity.high.svg" alt="">
+        <img class="data__upper__icon__image" :src="require(`@/assets/icons/${variable}.high.svg`)" alt="">
       </div>
       <div class="data__upper__button">
         <div class="data__upper__button__label">Lihat publikasi</div>
@@ -10,15 +10,18 @@
       </div>
     </div>
     <div class="data__lower">
-      <div class="data__lower__title">Curah hujan</div>
-      <div class="data__lower__content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatibus sit similique autem? Unde et saepe fugiat veritatis, libero rerum. Vero minima placeat beatae numquam autem esse illo assumenda. Animi!</div>
+      <div class="data__lower__title">{{ this.$t('variable.main.' + variable + '.name') }}</div>
+      <div class="data__lower__content">{{ this.$t('variable.main.' + variable + '.desc') }}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'variableCard'
+  name: 'variableCard',
+  props: {
+    variable: String
+  }
 }
 </script>
 
@@ -63,7 +66,7 @@ export default {
     align-items: flex-start;
     &__title {
       font-family: SFRoundedB;
-      font-size: 36px;
+      font-size: 32px;
       margin-block-end: 12px;
     }
     &__content {
--- src/pages/VariablePage.vue
+++ src/pages/VariablePage.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="page">
-    <VariableCard class="page__card"></VariableCard>
-    <VariableCard class="page__card"></VariableCard>
-    <VariableCard class="page__card"></VariableCard>
+    <div class="page__variable">
+      <VariableCard 
+        v-for="variable in variables" 
+        :variable="variable"
+        :key="variable"
+        class="page__variable__card"></VariableCard>
+    </div>
   </div>
 </template>
 
@@ -11,6 +15,17 @@ import VariableCard from '@/components/VariableCard.vue';
 
 export default {
   name: 'variablePage',
+  data () {
+    return {
+      variables: {
+        rainfall: 'rainfall',
+        temperature: 'temperature',
+        humidity: 'humidity',
+        sunshine: 'sunshine',
+        windspeed: 'windspeed'
+      }
+    }
+  },
   components: {
     VariableCard
   }
@@ -25,9 +40,15 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  &__card {
-    zoom: 80%;
-    margin: 18px;
+  &__variable {
+    width: 1046.4px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    &__card {
+      zoom: 80%;
+      margin: 18px;
+    }
   }
 }
 </style>
