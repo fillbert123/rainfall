c75ea2bb9329b1799ccffebd1183be68319dde9b
Fillbert <fillbert.susanto@gdn-commerce.com>
2023-06-12 11:12:32 +0700

    VariablePage added

--- src/pages/VariablePage.vue
+++ src/pages/VariablePage.vue
@@ -1,13 +1,33 @@
 <template>
-
+  <div class="page">
+    <VariableCard class="page__card"></VariableCard>
+    <VariableCard class="page__card"></VariableCard>
+    <VariableCard class="page__card"></VariableCard>
+  </div>
 </template>
 
 <script>
+import VariableCard from '@/components/VariableCard.vue';
+
 export default {
-  name: 'variablePage'
+  name: 'variablePage',
+  components: {
+    VariableCard
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.page {
+  background-color: var(--dark);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__card {
+    zoom: 80%;
+    margin: 18px;
+  }
+}
 </style>
